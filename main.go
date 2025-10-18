66da66d9cf5a07d9734781199ac8bf44042d91bb
Matt Horst <mattahorst@gmail.com>
2025-10-17 23:36:29 -0600

    serves index.html

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	var fileSystem http.Dir = "."
+	fileServer := http.FileServer(fileSystem)
+	mux.Handle("/", fileServer)
+
+
 	server := http.Server {Addr: ":8080", Handler: mux}
 
 	err := server.ListenAndServe()
