9e47fdcbe60b48f90c539a69685ce0ccc6b7cf93
Matt Horst <mattahorst@gmail.com>
2025-10-18 23:03:12 -0600

    adds heath endpoint

--- main.go
+++ main.go
@@ -5,13 +5,24 @@ import (
 	"fmt"
 )
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	var fileSystem http.Dir = "."
 	fileServer := http.FileServer(fileSystem)
-	mux.Handle("/", fileServer)
+	fileServer = http.StripPrefix("/app", fileServer)
+	mux.Handle("/app/", fileServer)
 
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	server := http.Server {Addr: ":8080", Handler: mux}
 
