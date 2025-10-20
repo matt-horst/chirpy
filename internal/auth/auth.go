8da4334068a73007845272fbdf480a60de3c5437
Matt Horst <mattahorst@gmail.com>
2025-10-20 17:56:57 -0600

    impls tests auth tests and creation and validation of JWTs

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,11 @@
 package auth
 
-import "github.com/alexedwards/argon2id"
+import (
+	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"time"
+)
 
 func HashPassword(password string) (string, error) {
 	return argon2id.CreateHash(password, argon2id.DefaultParams)
@@ -8,4 +13,41 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(password, hash string) (bool, error) {
 	return argon2id.ComparePasswordAndHash(password, hash)
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	now := time.Now()
+
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.RegisteredClaims {
+			Issuer: "chirpy",
+			IssuedAt: jwt.NewNumericDate(now), 
+			ExpiresAt: jwt.NewNumericDate(now.Add(expiresIn)),
+			Subject: userID.String(),
+		}, 
+	)
+
+	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&jwt.RegisteredClaims{},
+		func(t *jwt.Token) (any, error) {
+			return []byte(tokenSecret), nil
+		},
+	)
+
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	id, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	return uuid.Parse(id)
 }
